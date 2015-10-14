2918138d926e9298d709a60c1fdea145f6849390
Robert Morris <obviouslyrob@gmail.com>
2015-10-14 18:51:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weathermachine::Application.config.secret_key_base = '50ad9e7a7e83b973bef065254be73550a573c66cf04f37bc4772d3621e6810ccfa160d27170b39d25b0fafbc8b478bcb6da523ca9aac61eff066b1c29c0deb5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Weathermachine::Application.config.session_store :cookie_store, key: '_weathermachine_session'
